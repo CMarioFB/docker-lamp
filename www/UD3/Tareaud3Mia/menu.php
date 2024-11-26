eb0c287d169509326cd49a50acc53a2dc64f29f7
CMarioFB <a21carlosfb@iessanclemente.net>
2024-11-26 12:44:30 +0100

    hasta 25-XI

---
+++ www/UD3/Tareaud3Mia/menu.php
@@ -0,0 +1,33 @@
+<nav class="col-md-3 col-lg-2 d-md-block bg-light sidebar">
+    <div class="position-sticky">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+                    Home
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="init.php">Inicializar (mysqli)</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="I2.php">Lista de usuarios (PDO)</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="usuarioFormulario.php">Nuevo usuario (PDO)</a>
+            </li>
+
+            
+            <li class="nav-item">
+                <a class="nav-link" href="tareas.php">
+                    Lista de tareas (mysqli)
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="nuevaForm.php">Nueva tarea</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="nuevaForm.php">Buscador de tareas (PDO)</a>
+            </li>
+        </ul>
+    </div>
+</nav>
